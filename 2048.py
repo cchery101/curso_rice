409416fda51196e0355e83b3d54d5f03b5551db3
Luis Fontes <luis2@zplot.com>
2014-06-10 10:37:08 +0200

    Funciona bastante bien pero no del todo

--- 2048.py
+++ 2048.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 """
 Clone of 2048 game.
 """
 
-import poc_2048_gui
+# import poc_2048_gui
 
 # Directions, DO NOT MODIFY
 UP = 1
@@ -22,97 +23,47 @@ def merge(line):
     """
     Helper function that merges a single row or column in 2048
     """
-    num_celdas = len(line)
-    old_line = line
-    we_have_finished = False
-    while not we_have_finished:
-        new_line = [0 for i in]
-        for i in range(0, num_celdas - 1):
-            # If cell contains 0, we shift to the left all cells at the right
-            if line[i] == 0:
-                for j in range(i, num_celdas - 1):
-                    line[j]
+
+    imax = len(line) - 1
+    good_line = list(line) # because tmp_line should be a real new list
+    hemos_hecho_algun_cambio = True
+    hemos_sumado = False
+    while hemos_hecho_algun_cambio:
+        hemos_hecho_algun_cambio = False
+        working_line = list(good_line)
+        for i in range(1, imax + 1):
+
+            primera_celda_llena = i - 1
+            for j in range(i - 1, -1, -1):
+                if good_line[j] == 0:
+                    primera_celda_llena = j - 1
+            if i > 1:
+                ultima_celda_vacia = primera_celda_llena +1
+            else:
+                ultima_celda_vacia = -1
+            if ultima_celda_vacia != -1:
+                working_line[ultima_celda_vacia] = good_line[i] # movemos la celda
+                working_line[i] = 0 # ponemos un cero en la celda que hemos dejado vacía
+                hemos_hecho_algun_cambio = True
+            good_line = list(working_line)
+        # Vamos a sumar si procede
+        if not hemos_sumado:
+            working_line = list(good_line)
+            for i in range(1, imax + 1):
+                if good_line[i] == good_line[i - 1]:
+                    # Procede sumar
+                    working_line[i - 1] = good_line[i - 1] + good_line[i]
+                    working_line[i] = 0
+                    hemos_hecho_algun_cambio = True
+                    hemos_sumado = True
+                good_line = working_line
+    return good_line
 
 
-
-
-
-
-
-
-
-
-
-    return []
-
-class TwentyFortyEight:
-    """
-    Class to run the game logic.
-    """
-
-    def __init__(self, grid_height, grid_width):
-        # replace with your code
-        pass
-
-    def reset(self):
-        """
-        Reset the game so the grid is empty.
-        """
-        # replace with your code
-        pass
-
-    def __str__(self):
-        """
-        Return a string representation of the grid for debugging.
-        """
-        # replace with your code
-        pass
-
-    def get_grid_height(self):
-        """
-        Get the height of the board.
-        """
-        # replace with your code
-        return 0
-
-    def get_grid_width(self):
-        """
-        Get the width of the board.
-        """
-        # replace with your code
-        return 0
-
-    def move(self, direction):
-        """
-        Move all tiles in the given direction and add
-        a new tile if any tiles moved.
-        """
-        # replace with your code
-        pass
-
-    def new_tile(self):
-        """
-        Create a new tile in a randomly selected empty
-        square.  The tile should be 2 90% of the time and
-        4 10% of the time.
-        """
-        # replace with your code
-        pass
-
-    def set_tile(self, row, col, value):
-        """
-        Set the tile at position row, col to have the given value.
-        """
-        # replace with your code
-        pass
-
-    def get_tile(self, row, col):
-        """
-        Return the value of the tile at position row, col.
-        """
-        # replace with your code
-        return 0
-
-
-poc_2048_gui.run_gui(TwentyFortyEight(4, 4))
-
+line_in = [2, 0, 2, 4]
+line_out = merge(line_in)
+print
+print line_in
+print line_out
+print
+print '---------'
